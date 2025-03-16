b4c1df6992664aefd80ac677d100d7d69481b7ef
SreehariNvklm <shnvklm2004@gmail.com>
2025-03-16 10:23:16 +0530

    Small UI Change

--- Verdict.py
+++ Verdict.py
@@ -9,4 +9,7 @@ user_input = st.text_input("Enter the case: ")
 btn = st.button("Fetch Case Files")
 if btn and user_input:
     query_obj = DB_Query()
-    st.write(query_obj.search_faiss(user_input,1))
+    container = st.container(border=True)
+    res = query_obj.search_faiss(user_input,1)
+    res = res.replace("File corrected:","")
+    container.write(res)
