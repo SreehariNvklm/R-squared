d197c288b5440c4d4b6d8dcd69b05cef3af2d781
SreehariNvklm <shnvklm2004@gmail.com>
2025-03-16 05:47:23 +0530

    Streamlit update

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
+from src.database.db_query import DB_Query
 import streamlit as st
 
 st.header("Project Verdict")
 st.write("Search case files:")
 
 user_input = st.text_input("Enter the case: ")
+btn = st.button("Fetch Case Files")
+if btn and user_input:
+    query_obj = DB_Query()
+    st.write(query_obj.search_faiss(user_input,1))
